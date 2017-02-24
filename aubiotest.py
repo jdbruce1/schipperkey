4ed00e2b36198ecb459f41ed1e9cdeb1857fbbec
Will Molter <williammolter2018@u.northwestern.edu>
2017-02-24 14:02:29 -0600

    aubio testing

---
+++ aubiotest.py
@@ -0,0 +1,47 @@
+import sys
+from aubio import source, pitch
+
+filename = "toy dataset/silent.wav"
+
+downsample = 1
+samplerate = 22050 // downsample
+if len( sys.argv ) > 2: samplerate = int(sys.argv[2])
+
+win_s = 4096 // downsample # fft size
+hop_s = 512  // downsample # hop size
+
+s = source(filename, samplerate, hop_s)
+samplerate = s.samplerate
+
+tolerance = 0.8
+
+pitch_o = pitch("yin", win_s, hop_s, samplerate)
+pitch_o.set_unit("midi")
+pitch_o.set_tolerance(tolerance)
+
+pitches = []
+confidences = []
+
+# total number of frames read
+total_frames = 0
+while True:
+    samples, read = s()
+    pitch = pitch_o(samples)[0]
+    #pitch = int(round(pitch))
+    confidence = pitch_o.get_confidence()
+    #if confidence < 0.8: pitch = 0.
+    print("%f %f %f" % (total_frames / float(samplerate), pitch, confidence))
+    pitches += [pitch]
+    confidences += [confidence]
+    total_frames += read
+    if read < hop_s: break
+
+if 0: sys.exit(0)
+
+#print pitches
+import os.path
+from numpy import array, ma
+import matplotlib.pyplot as plt
+
+plt.plot(pitches)
+plt.show()
